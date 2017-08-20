09dfcca84299485317323432c957807a14c011b1
Jack Bittiner <jackbittiner@hotmail.com>
2017-08-20 14:38:11 +0100

    Display one bit of mock gif data

--- index.ios.js
+++ index.ios.js
@@ -1,52 +1,41 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
+
+var MOCKED_GIF_DATA = [
+  {gifurl: "https://media.giphy.com/media/WuGSL4LFUMQU/giphy.gif"},
+  {gifurl: "https://media.giphy.com/media/l4Ki2UBDUhRjvmC7m/giphy.gif"},
+  {gifurl: "https://media.giphy.com/media/ului10cR0BK8w/giphy.gif"}
+];
 
 export default class JiggyNativeAttempt extends Component {
   render() {
+    var gif = MOCKED_GIF_DATA[0];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Image
+        source={{uri: gif.gifurl}}
+        style={styles.gifurl} />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  gifurl: {
+    width: 53,
+    height: 81,
   },
 });
 
